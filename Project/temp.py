2a5675b139a39add140361ced86592fc2b8c6309
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2020-06-09 15:39:13 +0900

    코로나 그래프 출력

--- Project/temp.py
+++ Project/temp.py
@@ -1,90 +1,40 @@
-# from matplotlib import pyplot as plt
-# from matplotlib import animation
-# import numpy as np
-# import random
-# import time
-# #
-# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-# from matplotlib.figure import Figure
-# import tkinter as Tk
-#
-# fig = plt.figure()     #figure(도표) 생성
-#
-#
-#
-# ax = plt.subplot(211, xlim=(0, 50), ylim=(0, 1024))
-# ax_2 = plt.subplot(212, xlim=(0, 50), ylim=(0, 512))
-#
-#
-# max_points = 50
-# max_points_2 = 50
-#
-#
-# line, = ax.plot(np.arange(max_points),
-#                 np.ones(max_points, dtype=np.float)*np.nan, lw=1, c='blue',ms=1)
-# line_2, = ax_2.plot(np.arange(max_points_2),
-#                 np.ones(max_points, dtype=np.float)*np.nan, lw=1,ms=1)
-#
-#
-# def init():
-#     return line
-# def init_2():
-#     return line_2
-#
-#
-# def animate(i):
-#     y = random.randint(0,1024)
-#     old_y = line.get_ydata()
-#     new_y = np.r_[old_y[1:], y]
-#     line.set_ydata(new_y)
-#     print(new_y)
-#     return line
-#
-# def animate_2(i):
-#     y_2 = random.randint(0,512)
-#     old_y_2 = line_2.get_ydata()
-#     new_y_2 = np.r_[old_y_2[1:], y_2]
-#     line_2.set_ydata(new_y_2)
-#     print(new_y_2)
-#     return line_2
-#
-#
-#
-#
-# root = Tk.Tk() #추가
-# label = Tk.Label(root,text="라벨").grid(column=0, row=0)#추가
-# canvas = FigureCanvasTkAgg(fig, master=root) #
-# canvas.get_tk_widget().grid(column=0,row=1) #
-#
-#
-#
-# #anim = animation.FuncAnimation(fig, animate  , init_func= init ,frames=200, interval=50, blit=False)
-# #anim_2 = animation.FuncAnimation(fig, animate_2  , init_func= init_2 ,frames=200, interval=10, blit=False)
-# Tk.mainloop()
-
-
-import tkinter as tk
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import HomeUI
+import CoronaStateXMLParsing as CS
+
+#한글 폰트 적용
+from matplotlib import font_manager, rc
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font_name)
 
 
-data2 = {'Year': [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010],
-         'Unemployment_Rate': [9.8, 12, 8, 7.2, 6.9, 7, 6.5, 6.2, 5.5, 6.3]
+data2 = {'날짜': [ CS.data[0]['date'], CS.data[1]['date'],  CS.data[2]['date'],  CS.data[3]['date'],  CS.data[4]['date'],  CS.data[5]['date'],  CS.data[6]['date']],
+         '일확진자': [int(CS.data[0]['getCorona'])-int(CS.data[1]['getCorona']), int(CS.data[1]['getCorona'])-int(CS.data[2]['getCorona']), int(CS.data[2]['getCorona'])-int(CS.data[3]['getCorona']),
+                              int(CS.data[3]['getCorona'])-int(CS.data[4]['getCorona']), int(CS.data[4]['getCorona'])-int(CS.data[5]['getCorona']), int(CS.data[5]['getCorona'])-int(CS.data[6]['getCorona']),
+                              int(CS.data[6]['getCorona'])-int(CS.data[7]['getCorona'])]
          }
-df2 = DataFrame(data2, columns=['Year', 'Unemployment_Rate'])
+df2 = DataFrame(data2, columns=['날짜', '일확진자'])
 
-
+df2=df2.astype({'일확진자':int})
 
 
 def makegrape(window):
-    figure2 = plt.Figure(figsize=(4, 4), dpi=80)
+    figure2 = plt.Figure(figsize=(6, 4), dpi=80)
     ax2 = figure2.add_subplot(111)
+    list = [i for i in range(0,100,2)]
+    ax2.set_yticks(list)
+
     line2 = FigureCanvasTkAgg(figure2, window)
     line2.get_tk_widget().place(x=600, y=150)
-    df21 = df2[['Year', 'Unemployment_Rate']].groupby('Year').sum()
-    df21.plot(kind='line', legend=True, ax=ax2, color='r', marker='o', fontsize=10)
-    ax2.set_title('Year Vs. Unemployment Rate')
+    df21 = df2[["날짜", "일확진자"]].groupby("날짜").sum()
+    df21.plot(kind='line', legend=True, ax=ax2, color='r', marker='o',fontsize=10)
+
+    ax2.set_yticklabels(list, fontsize=15)
+    ax2.set_xlabel("날짜",fontsize=12,weight='bold')
+    ax2.set_ylabel("일확진자",fontsize=12,weight='bold')
+
+
+    ax2.set_title("최근 7일 일일확진자",fontsize=20,weight='bold')
 
 
