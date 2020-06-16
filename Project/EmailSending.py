bc9f347913408af8a12d7d34444214f75131db0b
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2020-06-17 06:01:20 +0900

    이메일 예시 완성

--- Project/EmailSending.py
+++ Project/EmailSending.py
@@ -1,14 +1,34 @@
-import smtplib
-from email.mime.text import MIMEText
+import smtplib, os, pickle  # smtplib: 메일 전송을 위한 패키지
+from email import encoders  # 파일전송을 할 때 이미지나 문서 동영상 등의 파일을 문자열로 변환할 때 사용할 패키지
+from email.mime.text import MIMEText   # 본문내용을 전송할 때 사용되는 모듈
+from email.mime.multipart import MIMEMultipart   # 메시지를 보낼 때 메시지에 대한 모듈
+from email.mime.base import MIMEBase
 
 me = 'dnk97@naver.com'
 you = 'dnk972001@kpu.ac.kr'
 contents = '안녕하세요'
 
-msg = MIMEText(contents, _charset='euc-kr')
+msg = MIMEMultipart()
 msg['Subject'] = '코로나종합상황센터에서 전송된 메일입니다.'
 msg['From'] = me
 msg['To'] = you
+
+
+
+html = MIMEText(
+                '''<html>
+                            <head></head>
+                            <body>
+                                Hi ${NAME}.<br>
+                                <img src="https://shopping-phinf.pstatic.net/main_8238249/82382493518.2.jpg?type=f200"><br>
+                                This is a test message.
+                            </body>
+                        </html>''','html')
+
+
+
+msg.attach(html)
+print(html)
 
 # 네이버 버전
 naver_server = smtplib.SMTP_SSL('smtp.naver.com', 465)
