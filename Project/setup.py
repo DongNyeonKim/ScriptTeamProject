536bc75b0dbfa9f8ab5c861cb1452e66505bf889
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2020-06-18 19:05:40 +0900

    배포파일 만들기

---
+++ Project/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="Covid-19_Control_Center",
+      version='1.0',
+      py_modules=['CoronaGraph','CoronaMaskList','CoronaStateXMLParsing','CrowlingShoppingList',
+                  'EmailSending','GetCoronaMap','HomeUI','MainTUI','MaskUI','SearchFunction',
+                  'ShoppingList','ShoppingUI','TelegramBot','TelegramStart'])
